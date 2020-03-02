c4e00e579516248aa37fe5e96bf9f3ab2d35e686
mana jalili <jalilimana@gmail.com>
2020-03-02 16:18:17 +0100

    added the methods in the policy

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -4,4 +4,17 @@ class EventPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
 end
