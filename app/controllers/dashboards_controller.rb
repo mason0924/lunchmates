f8b462bc92a32c471d13fccc10044b672ccf21ba
azadeharam <aram.azadeh@yahoo.com>
2020-03-03 14:36:55 +0100

    dashboards is done partially

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,11 +4,15 @@ class DashboardsController < ApplicationController
   def index
     @bookings = policy_scope(Booking)
     @events = Event.where(user_id: current_user[params[:id]])
-
   end
 
   def event
     @hosting_events = current_user.events
     @joining_events = current_user.bookings
+  end
+
+  def show
+    @booking = Booking.where(user_id: current_user[params[:id]])
+    @event =Event.where(user_id: current_user[params[:id]])
   end
 end
