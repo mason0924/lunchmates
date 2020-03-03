933a57712b82bbb2404d723d5b13dd786d8b87b3
azadeharam <aram.azadeh@yahoo.com>
2020-03-03 14:49:47 +0100

    dashboards has changed to dashboard and controller been cleaned up

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,18 +1,9 @@
 class DashboardsController < ApplicationController
     # before_action :authenticate_user!
 
-  def index
-    @bookings = policy_scope(Booking)
-    @events = Event.where(user_id: current_user[params[:id]])
-  end
-
-  def event
-    @hosting_events = current_user.events
-    @joining_events = current_user.bookings
-  end
-
   def show
-    @booking = Booking.where(user_id: current_user[params[:id]])
-    @event =Event.where(user_id: current_user[params[:id]])
+    @bookings = current_user.bookings
+    @events = current_user.events
+    skip_authorization
   end
 end
