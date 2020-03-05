a294cebc172d04daff17093dfe841db06dd3206c
mason tan <masonev@gmail.com>
2020-03-05 19:27:51 +0100

    added logo and changed Avatar profile pic to user

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -26,7 +26,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 100, duration: 2500 });
 };
 
 const initMapbox = () => { //this is the default from mapbox
