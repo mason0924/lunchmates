75006bde73e9a14389a7bb147909706dec8aab97
mason tan <masonev@gmail.com>
2020-03-05 17:35:48 +0100

    fixed javascript and map and show pictures in map and show pages

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,11 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
 
-flatpickr(".datepicker", {})
+function initFlatpickr() {
+  const element = document.querySelector(".datepicker");
+  if (element) {
+    flatpickr(".datepicker", {});
+  }
+}
+
+export { initFlatpickr }
