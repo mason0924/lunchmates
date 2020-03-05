5d659b87e9f52c8e7b491564697cf9599001e3d7
azadeharam <aram.azadeh@yahoo.com>
2020-03-05 15:08:37 +0100

    try to write

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,10 @@
 import "bootstrap";
-
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import "../plugins/flatpickr"
 
 initMapbox();
 initAutocomplete();
