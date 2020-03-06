4495ed06b90513ee888782ffe3fbc906b776d894
mason tan <masonev@gmail.com>
2020-03-06 16:03:19 +0100

    added lots of seed!!

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,8 @@ user_2 = User.create!( first_name: "Mana", last_name: "Jalili", email:"mana@gmai
 user_3 = User.create!( first_name: "Mattia", last_name: "Macor", email:"mattia@gmail.com", password: "123456" )
 user_4 = User.create!( first_name: "Mason", last_name: "Tan", email:"mason@gmail.com", password: "123456" )
 user_5 = User.create!( first_name: "Caro", last_name: "Jen", email:"caro@gmail.com", password: "123456" )
-user_6 = User.create!( first_name: "Ebony", last_name: "Cole", email:"ebony@gmail.com", password: "123456" )
-user_7 = User.create!( first_name: "Riri", last_name: "King", email:"riri@gmail.com", password: "123456" )
+user_6 = User.create!( first_name: "Emma", last_name: "Cole", email:"ebony@gmail.com", password: "123456" )
+user_7 = User.create!( first_name: "Reyrey", last_name: "King", email:"riri@gmail.com", password: "123456" )
 user_8 = User.create!( first_name: "Raja", last_name: "Kash", email:"raja@gmail.com", password: "123456" )
 user_9 = User.create!( first_name: "Tako", last_name: "Hiroki", email:"tako@gmail.com", password: "123456" )
 user_10 = User.create!( first_name: "Sam", last_name: "Smith", email:"sam@gmail.com", password: "123456" )
@@ -45,17 +45,17 @@ puts 'Creating Events'
 # Event
 event_1 = Event.create!( user: user_13, event_name: "Pasta time!", description: "My office is a block away from Vapiano. And I love pasta. Do you? 🍝",  restaurant_name: "Vapiano", restaurant_address: "Friedrichstraße 50-55, 10117 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 12:00"), end_time: DateTime.parse("06/03/2020 13:00") , spots: 4 )
 event_2 = Event.create!( user: user_12, event_name: "KFC lovers", description: "I love fried chickens! Finger LIckin' Good👅",   restaurant_name: "KFC", restaurant_address: "Friedrichstraße 45, 10969 Berlin", restaurant_price_range: 1, start_time: DateTime.parse("06/03/2020 12:00"), end_time: DateTime.parse("06/03/2020 13:00"), spots: 4 )
-event_3 = Event.create!( user: user_3, event_name: "Ishin lovers🍣", description: "Their service is kind of rude but maybe because I was alone...Would you join me?" , restaurant_name: " Ishin ", restaurant_address: " Charlottenstraße 16, 10117 Berlin ", restaurant_price_range: 1, start_time: DateTime.parse("06/03/2020 13:00"), end_time: DateTime.parse("06/03/2020 13:30"), spots: 4 )
-event_4 = Event.create!( user: user_8, event_name: "I'm lovin🍟 ", description: "I love McNuggets. No judgement!" , restaurant_name: "McDonald's", restaurant_address: "Friedrichstraße 207, 10969 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 14:00"), end_time: DateTime.parse("06/03/2020 15:00"), spots: 3 )
-event_5 = Event.create!( user: user_19, event_name: "Syrian sandwich", description: "Hi, I'm new in Berlin and looking for friends. This Syrian place is pretty authentic. Please join me if you are interested. Thanks." , restaurant_name: " Yorak ", restaurant_address: " Wilhelmstraße 41A, 10963 Berlin ", restaurant_price_range: 1, start_time: DateTime.parse("06/03/2020 14:00"), end_time: DateTime.parse("06/03/2020 14:45"), spots: 5 )
-event_6 = Event.create!( user: user_5, event_name: "Spicy noodles🍜", description: "I love spicy soup noodles!" , restaurant_name: "Liu Noodles House ", restaurant_address: "Kronenstraße 72, 10117 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 14:00"), end_time: DateTime.parse("06/03/2020 15:00"), spots: 5 )
-event_7 = Event.create!( user: user_8, event_name: "Johny's chip in", description: "let's go to Jony's" , restaurant_name: "  Johny's", restaurant_address: " Markgrafenstraße 56, 10117 Berlin ", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 14:00"), end_time: DateTime.parse("06/03/2020 15:30"), spots: 3 )
-event_8 = Event.create!( user: user_9, event_name: "Bocca di Bacco fans", description: "Meet me at Bocca di Bacco" , restaurant_name: " Bocca di Bacco ", restaurant_address: " Friedrichstraße 167-168, 10117 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 15:00"), end_time: DateTime.parse("06/03/2020 15:45"), spots: 5 )
-event_9 = Event.create!( user: user_10, event_name: "QIU Restaurant lovers", description: "let's meet up at QIU - Bar & Restaurant" , restaurant_name: " QIU - Bar & Restaurant", restaurant_address: " Potsdamer Straße 3, 10785 Berlin ", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 15:00"), end_time: DateTime.parse("06/03/2020 16:00"), spots: 4 )
-event_10 = Event.create!( user: user_11, event_name: "Salad and chat", description: "I have an hour for lunch break, would be nice if someone can join me :-)" , restaurant_name: "Little Green Rabbit", restaurant_address: "Friedrichstraße 200, 10117 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 15:00"), end_time: DateTime.parse("06/03/2020 16:30"), spots: 4 )
-event_11 = Event.create!( user: user_1, event_name: "Maximilians lovers", description: "It's very touristy and fun. Beers are amazing!",  restaurant_name: " Maximilians ", restaurant_address: "  Friedrichstraße 185-190, 10117 Berlin ", restaurant_price_range: 3, start_time: DateTime.parse("06/03/2020 16:00"), end_time: DateTime.parse("06/03/2020 17:00"), spots: 5 )
-event_12 = Event.create!( user: user_8, event_name: "Exchange student from India 🍛", description: "Hi my name is Raja. I'm an exchange student from India and new in Berlin. I usually come here to have lunch during the lunch time. I can help translating the menu! :)",  restaurant_name: " Delhi 6 ", restaurant_address: "Friedrichstraße 237, 10969 Berlin", restaurant_price_range: 1, start_time: DateTime.parse("06/03/2020 17:00"), end_time: DateTime.parse("06/03/2020 17:30"), spots: 5 )
-event_13 = Event.create!( user: user_8, event_name: "Business lunch💼", description: "On a business trip in Berlin. Join me if you are interested to have a quick lunch",  restaurant_name: "SOLAR Bar Restaurant Lounge", restaurant_address: "Stresemannstraße 76, 10963 Berlin", restaurant_price_range: 5, start_time: DateTime.parse("06/03/2020 16:00"), end_time: DateTime.parse("06/03/2020 17:00"), spots: 2 )
+event_3 = Event.create!( user: user_9, event_name: "Ishin lovers🍣", description: "Their service is kind of rude but maybe because I was alone...Would you join me?" , restaurant_name: " Ishin ", restaurant_address: " Charlottenstraße 16, 10117 Berlin ", restaurant_price_range: 1, start_time: DateTime.parse("06/03/2020 13:00"), end_time: DateTime.parse("06/03/2020 13:30"), spots: 4 )
+event_4 = Event.create!( user: user_19, event_name: "Syrian sandwich", description: "Hi, I'm new in Berlin and looking for friends. This Syrian place is pretty authentic. Please join me if you are interested. Thanks." , restaurant_name: " Yorak ", restaurant_address: " Wilhelmstraße 41A, 10963 Berlin ", restaurant_price_range: 1, start_time: DateTime.parse("06/03/2020 14:00"), end_time: DateTime.parse("06/03/2020 14:45"), spots: 5 )
+event_5 = Event.create!( user: user_5, event_name: "Spicy noodles🍜", description: "I love spicy soup noodles!" , restaurant_name: "Liu Noodles House ", restaurant_address: "Kronenstraße 72, 10117 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 14:00"), end_time: DateTime.parse("06/03/2020 15:00"), spots: 5 )
+event_6 = Event.create!( user: user_7, event_name: "Johny's chip in", description: "let's go to Jony's" , restaurant_name: "  Johny's", restaurant_address: " Markgrafenstraße 56, 10117 Berlin ", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 14:00"), end_time: DateTime.parse("06/03/2020 15:30"), spots: 3 )
+event_7 = Event.create!( user: user_6, event_name: "Bocca di Bacco fans", description: "Meet me at Bocca di Bacco" , restaurant_name: " Bocca di Bacco ", restaurant_address: " Friedrichstraße 167-168, 10117 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 15:00"), end_time: DateTime.parse("06/03/2020 15:45"), spots: 5 )
+event_8 = Event.create!( user: user_2, event_name: "QIU Restaurant lovers", description: "let's meet up at QIU - Bar & Restaurant" , restaurant_name: " QIU - Bar & Restaurant", restaurant_address: " Potsdamer Straße 3, 10785 Berlin ", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 15:00"), end_time: DateTime.parse("06/03/2020 16:00"), spots: 4 )
+event_9 = Event.create!( user: user_11, event_name: "Salad and chat", description: "I have an hour for lunch break, would be nice if someone can join me :-)" , restaurant_name: "Little Green Rabbit", restaurant_address: "Friedrichstraße 200, 10117 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 15:00"), end_time: DateTime.parse("06/03/2020 16:30"), spots: 4 )
+event_10 = Event.create!( user: user_20, event_name: "Maximilians lovers", description: "It's very touristy and fun. Beers are amazing!",  restaurant_name: " Maximilians ", restaurant_address: "  Friedrichstraße 185-190, 10117 Berlin ", restaurant_price_range: 3, start_time: DateTime.parse("06/03/2020 16:00"), end_time: DateTime.parse("06/03/2020 17:00"), spots: 5 )
+event_11 = Event.create!( user: user_8, event_name: "Exchange student from India 🍛", description: "Hi my name is Raja. I'm an exchange student from India and new in Berlin. I usually come here to have lunch during the lunch time. I can help translating the menu! :)",  restaurant_name: " Delhi 6 ", restaurant_address: "Friedrichstraße 237, 10969 Berlin", restaurant_price_range: 1, start_time: DateTime.parse("06/03/2020 17:00"), end_time: DateTime.parse("06/03/2020 17:30"), spots: 5 )
+event_12 = Event.create!( user: user_14, event_name: "Business lunch💼", description: "On a business trip in Berlin. Join me if you are interested to have a quick lunch",  restaurant_name: "SOLAR Bar Restaurant Lounge", restaurant_address: "Stresemannstraße 76, 10963 Berlin", restaurant_price_range: 5, start_time: DateTime.parse("06/03/2020 16:00"), end_time: DateTime.parse("06/03/2020 17:00"), spots: 2 )
+event_13 = Event.create!( user: user_16, event_name: "I'm lovin🍟 ", description: "I love McNuggets. No judgement!" , restaurant_name: "McDonald's", restaurant_address: "Friedrichstraße 207, 10969 Berlin", restaurant_price_range: 2, start_time: DateTime.parse("06/03/2020 14:00"), end_time: DateTime.parse("06/03/2020 15:00"), spots: 3 )
 
 events = [event_1, event_2, event_3, event_4, event_5, event_6, event_7, event_8, event_9, event_10, event_11, event_12, event_13]
 images_events = [
@@ -78,16 +78,81 @@ images_events.each_with_index do |image, index|
   events[index].photos.attach(io: file, filename: 'restaurantpic.jpg', content_type: 'image/jpg')
 end
 
+event_1.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583406406/Restaurant%20Picutres/Vapiano/hcmp131298_447734_l1x2pc.jpg"), filename: 'restaurantpic2.jpg', content_type: 'image/jpg')
+event_1.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583406362/Restaurant%20Picutres/Vapiano/vapianocentre_oj8mw9.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_1.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583406325/Restaurant%20Picutres/Vapiano/cover_image.jpg.640x360_q85_crop_hnezfx.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_2.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504470/Restaurant%20Picutres/_107847262_gettyimages-1152276563-594x594_o1se3p.jpg"), filename: 'kfc.jpg', content_type: 'image/jpg')
+event_2.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504471/Restaurant%20Picutres/5d0aa51197ad7643080a8af3_yfw2rt.jpg"), filename: 'kfc2.jpg', content_type: 'image/jpg')
+event_3.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504663/Restaurant%20Picutres/220px-Sushi_platter_er6ni9.jpg"), filename: 'sushi.jpg', content_type: 'image/jpg')
+event_3.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504668/Restaurant%20Picutres/kimbap-korean-sushi-rolls-2118795-Hero-5b7dbdd346e0fb00250718b8_gzzfmp.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_4.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504658/Restaurant%20Picutres/screen-shot-2019-08-29-at-2.05.53-pm-816x588_d1y48v.png"), filename: 'syrian.jpg', content_type: 'image/jpg')
+event_4.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504654/Restaurant%20Picutres/c3ec81dde642f5b7ac070c97a83d0225_zvlqbg.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_5.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504694/Restaurant%20Picutres/Liu-Berlin-_E6_88_90_E9_83_BD_E5_91_B3_E9_81_93-Chengdu-Noodles-Exterior-1024x683_ujrfwc.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_5.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504686/Restaurant%20Picutres/photo1jpg_uvm9tb.jpg"), filename: 'noodle.jpg', content_type: 'image/jpg')
+event_6.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504832/Restaurant%20Picutres/traditional-german-food-pork-knuckles-currywurst_oz4wzu.jpg"), filename: 'rp1.jpg', content_type: 'image/jpg')
+event_6.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504840/Restaurant%20Picutres/Schnitzel_qfryve_b8ob2u.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_7.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504928/Restaurant%20Picutres/Bocca_di_bacco_2-fettuccine_a_la_bolognese_bvuclv.jpg"), filename: 'rp1.jpg', content_type: 'image/jpg')
+event_7.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504939/Restaurant%20Picutres/6a5349_0c5747c8de9148c685b26901bb84a0c0_mv2_d_4032_3024_s_4_2_ky6fwq.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_8.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504961/Restaurant%20Picutres/110b106c7c5af9b694f6c8f45775bdd7_zftixg.jpg"), filename: 'rp1.jpg', content_type: 'image/jpg')
+event_8.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583504982/Restaurant%20Picutres/jin-qiu-129-vista-sala-055a8_kgbhdl.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_9.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505010/Restaurant%20Picutres/mozzarella-avocado-salad_tutjff.jpg"), filename: 'rp1.jpg', content_type: 'image/jpg')
+event_9.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505039/Restaurant%20Picutres/Little-Green-Rabbit-Couscous-Salat-1_rp2qoa.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_10.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505085/Restaurant%20Picutres/Food-from-Germany_v5bcs8.jpg"), filename: 'rp1.jpg', content_type: 'image/jpg')
+event_10.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505128/Restaurant%20Picutres/gn-gift_guide_variable_c_ea3iwx.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_11.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505246/Restaurant%20Picutres/the-best-top-10-indian-dishes-865x647-731x547_biwlan.jpg"), filename: 'rp1.jpg', content_type: 'image/jpg')
+event_11.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505270/Restaurant%20Picutres/p07cj8zj_ky7krg.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_12.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505386/Restaurant%20Picutres/business-lunch_rgyuar.jpg"), filename: 'rp1.jpg', content_type: 'image/jpg')
+event_12.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505572/Restaurant%20Picutres/easy-lunches-for-stressed-out-people-400_wozstt.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+event_13.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505444/Restaurant%20Picutres/shutterstock_243788887-1024x1024_glvmr9.jpg"), filename: 'rp1.jpg', content_type: 'image/jpg')
+event_13.photos.attach(io: URI.open("https://res.cloudinary.com/lunchmates/image/upload/v1583505475/Restaurant%20Picutres/McDonald_27s_2C_6875_Sand_Lake_Rd_2C_Orlando_interior_2_xtursd.jpg"), filename: 'rp3.jpg', content_type: 'image/jpg')
+
+
+
 
 puts 'Creating Bookings'
 # Booking
 booking_1 = Booking.create!( event: event_1, user: user_16 ) #pasta
-booking_2 = Booking.create!( event: event_1, user: user_1 )
-booking_3 = Booking.create!( event: event_1, user: user_2 )
-booking_4 = Booking.create!( event: event_3, user: user_4 ) #KFC
-booking_5 = Booking.create!( event: event_3, user: user_5 )
-booking_6 = Booking.create!( event: event_3, user: user_6 )
-booking_7 = Booking.create!( event: event_3, user: user_9 )
+booking_2 = Booking.create!( event: event_1, user: user_6 )
+
+booking_4 = Booking.create!( event: event_2, user: user_4 ) #KFC
+
+booking_5 = Booking.create!( event: event_3, user: user_1 ) #ishin
+booking_6 = Booking.create!( event: event_3, user: user_2 )
+booking_7 = Booking.create!( event: event_3, user: user_3 )
+
+booking_3 = Booking.create!( event: event_4, user: user_20 )
+booking_8 = Booking.create!( event: event_4, user: user_19 )#syrian
+booking_9 = Booking.create!( event: event_4, user: user_9 )
+
+booking_10 = Booking.create!( event: event_5, user: user_1 )#noodle
+booking_11 = Booking.create!( event: event_5, user: user_9 )
+# booking_12 = Booking.create!( event: event_5, user: user_9 )
+
+booking_13 = Booking.create!( event: event_6, user: user_10 )#jonny
+booking_14 = Booking.create!( event: event_6, user: user_19 )
+
+booking_15 = Booking.create!( event: event_7, user: user_19 )#bocca
+booking_16 = Booking.create!( event: event_7, user: user_9 )
+
+booking_17 = Booking.create!( event: event_8, user: user_17 )#qiu
+booking_18 = Booking.create!( event: event_8, user: user_19 )
+
+booking_19 = Booking.create!( event: event_9, user: user_7 )#salad
+booking_20 = Booking.create!( event: event_9, user: user_9 )
+
+booking_21 = Booking.create!( event: event_10, user: user_18 )#maxi
+booking_22 = Booking.create!( event: event_10, user: user_2 )
+
+booking_23 = Booking.create!( event: event_11, user: user_20 )#india
+booking_24 = Booking.create!( event: event_11, user: user_11 )
+
+booking_25 = Booking.create!( event: event_12, user: user_9 )#busine
+# booking_26 = Booking.create!( event: event_12, user: user_9 )
+
+booking_27 = Booking.create!( event: event_13, user: user_9 )#mcd
+# booking_28 = Booking.create!( event: event_13, user: user_9 )
+
+
 
 users = [user_1, user_2, user_3, user_4, user_5, user_6, user_7, user_8, user_9, user_10, user_11, user_12, user_13, user_14, user_15, user_16, user_17, user_18, user_19, user_20]
 
