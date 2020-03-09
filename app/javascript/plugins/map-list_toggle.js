ff422f15b0efd84a1d230d7ff42e9c4696182855
Mattia Macor <mtt112358@gmail.com>
2020-03-09 18:43:07 +0100

    fixed map

--- app/javascript/plugins/map-list_toggle.js
+++ app/javascript/plugins/map-list_toggle.js
@@ -2,7 +2,8 @@ const toggleMap = () => {
   const map = document.getElementById("map");
   const mapListButton = document.getElementById("map-list");
   const list = document.getElementById("list");
-
+  if (mapListButton) {
+    map.classList.add("hidden");
     mapListButton.addEventListener('click', (event) => {
       if (mapListButton.innerText === "Map") {
         map.classList.remove("hidden");
@@ -13,7 +14,8 @@ const toggleMap = () => {
         list.classList.remove("hidden");
         mapListButton.innerText = "Map";
       };
-  });
+   });
+  }
 }
 
 export {toggleMap}
