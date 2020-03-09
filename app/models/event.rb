7f68728d0051d56a0ea5668241761f2fda094a63
azadeharam <aram.azadeh@yahoo.com>
2020-03-09 13:57:50 +0100

    working on validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,5 @@ class Event < ApplicationRecord
   geocoded_by :restaurant_address
   after_validation :geocode, if: :will_save_change_to_restaurant_address?
   has_many_attached :photos
+  validates :spots, :inclusion => 1..10
 end
