05da22336de48a39126612ade74ec49c8b29f022
Mattia Macor <mtt112358@gmail.com>
2020-03-09 18:03:04 +0100

    map button

---
+++ app/javascript/plugins/map-list_toggle.js
@@ -0,0 +1,19 @@
+const toggleMap = () => {
+  const map = document.getElementById("map");
+  const mapListButton = document.getElementById("map-list");
+  const list = document.getElementById("list");
+
+    mapListButton.addEventListener('click', (event) => {
+      if (mapListButton.innerText === "Map") {
+        map.classList.remove("hidden");
+        list.classList.add("hidden")
+        mapListButton.innerText = "List";
+      } else if (mapListButton.innerText === "List") {
+        map.classList.add("hidden");
+        list.classList.remove("hidden");
+        mapListButton.innerText = "Map";
+      };
+  });
+}
+
+export {toggleMap}
