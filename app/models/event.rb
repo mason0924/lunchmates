47472ff8c1cb7b05df62ba1e1eb33e18f70ed100
mason tan <masonev@gmail.com>
2020-03-10 10:56:41 +0100

    fix seed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :messages
+  has_many :messages, dependent: :destroy
   geocoded_by :restaurant_address
   after_validation :geocode, if: :will_save_change_to_restaurant_address?
   has_many_attached :photos
