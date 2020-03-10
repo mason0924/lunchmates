1f55ff4b7e48ab16bba91cb4ea46ae9b6673c0c0
mana jalili <jalilimana@gmail.com>
2020-03-10 15:10:35 +0100

    added validation for restaurant name and address

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,8 @@ class Event < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_restaurant_address?
   has_many_attached :photos
   validates :event_name, presence: true
+  validates :restaurant_address, presence: true
+  validates :restaurant_name, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :spots, presence: true
