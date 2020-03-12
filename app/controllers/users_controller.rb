ff360e4b99b7857e11bfbd44468428eaa3b2182f
mason tan <masonev@gmail.com>
2020-03-13 00:51:51 +0100

    seed and preference for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,23 @@ class UsersController < ApplicationController
     authorize @user
   end
 
+  def update
+    @user = User.find(params[:id])
+    preference = @user.preference
+    authorize @user
+    if @user.update(user_preference)
+      redirect_to edit_user_registration_path
+    else
+      render :edit
+    end
+    # Only for update, create and destroy you write here the paths of the buttons
+  end
+
   def user_params
     params.require(:user).permit(:first_name)
+  end
+
+  def user_preference
+    params.require(:user).permit(:preference)
   end
 end
