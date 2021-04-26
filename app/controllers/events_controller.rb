eee665eac6f6427a39a5f3e9c53bf52db5a80fb0
mason tan <masonev@gmail.com>
2021-04-25 20:50:45 -0400

    users can view events without logged in

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  #this show events without logged in :-D
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @events = policy_scope(Event)
